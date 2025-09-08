23711765fc2e3f1ffd0462003180648d5d94804c
dabweezy <168998991+dabweezy@users.noreply.github.com>
2025-09-08 22:14:14 +0100

    bottom tab

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -23,7 +23,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   }, [initializeSampleData])
 
   return (
-    <div className="h-screen flex overflow-hidden bg-blue-600">
+    <div className="h-screen flex overflow-hidden bg-blue-600 p-1.5">
       {/* Desktop Sidebar */}
       <div className="hidden lg:flex">
         <Sidebar />
@@ -79,10 +79,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
 
         {/* Main Content Container */}
         <motion.div 
-          className={clsx(
-            'flex-1 flex flex-col transition-all duration-300 ease-in-out',
-            'lg:mt-1.5 lg:mr-1.5 lg:mb-1.5'
-          )}
+          className="flex-1 flex flex-col transition-all duration-300 ease-in-out"
         >
           <div className="flex-1 bg-white lg:rounded-4xl shadow-timestack relative flex flex-col">
             {/* Header */}
